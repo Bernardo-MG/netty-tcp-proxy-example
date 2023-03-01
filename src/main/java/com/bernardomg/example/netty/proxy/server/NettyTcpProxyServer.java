4ba739f3ea2cc74f60ed3e88399e29e8e2d968c2
Bernardo-MG <programming@bernardo-mg.com>
2023-03-01 13:30:34 +0100

    Added comment

--- src/main/java/com/bernardomg/example/netty/proxy/server/NettyTcpProxyServer.java
+++ src/main/java/com/bernardomg/example/netty/proxy/server/NettyTcpProxyServer.java
@@ -128,6 +128,11 @@ public final class NettyTcpProxyServer implements Server {
         log.trace("Stopped proxy");
     }
 
+    /**
+     * Starts a server connection and returns a channel.
+     * 
+     * @return channel for the server
+     */
     private final Channel connectoToServer() {
         final ServerBootstrap bootstrap;
         final ChannelFuture   channelFuture;
